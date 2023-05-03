936682fd49100444536b3b4ec96e2a1707bc335a
IT21813184-kavi <it21813184@my.sliit.lk>
2023-05-03 23:38:04 +0530

    commit

---
+++ src/main/java/com/oop/servlet/loadPaymentServlet.java
@@ -0,0 +1,20 @@
+package com.oop.servlet;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+/**
+ * Servlet implementation class loadPaymentServlet
+ */
+public class loadPaymentServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public loadPaymentServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+}
